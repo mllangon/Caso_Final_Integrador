df1e397993a496e52fbf15af20862fb7b9bd77b5
mllangon <mllangon@myuax.com>
2023-11-28 12:15:20 +0100

    2

--- 1_Variant/Variant.cpp
+++ 1_Variant/Variant.cpp
@@ -3,9 +3,6 @@
 #include <string>
 #include <vector>
 #include <sstream>
-#include <llans/json.hpp>
-
-using json = nlohmann::json;
 
 class Variant{
 public:
@@ -13,6 +10,18 @@ public:
     explicit Variant(double num) : value(num), type(Type::Number) {}
     explicit Variant(std::string str) : value(str), type(Type::String) {}
     explicit Variant(std::vector<Variant> list) : value(list), type(Type::List) {}
+
+    std::string to_string() const {
+        switch (type) {
+            case Type::Number:
+                return std::to_string(std::get<double>(value));
+            case Type::String:
+                return std::get<std::string>(value);
+            case Type::List:
+                return list_to_string();
+        }
+        return "";
+    }
 
 
 };
